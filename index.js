04cbdaa1a2ce0b55164d409618ca4bc2a03f6ba8
thomas demoncy <demoncy.thomas@gmail.com>
2023-09-06 12:34:06 +0200

    added a few comments to make the results more readable.

--- index.js
+++ index.js
@@ -41,6 +41,10 @@ if (hacker1 < hacker2){
     console.log("What?! You both have the same name?");
 };
 
+
+// Bonus Time!: 
+
+// Bonus 1:
 const longText= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum posuere turpis et vestibulum sollicitudin. Praesent luctus ex sit amet leo rhoncus, et tristique lorem tincidunt. Sed erat mauris, dapibus id tellus eget, dictum faucibus purus. Proin pulvinar lobortis dui eu consectetur. Nunc ut accumsan enim. Phasellus rhoncus orci ut nunc finibus, a cursus quam dictum. Ut ac est felis. Pellentesque eget augue non enim pretium sodales sit amet sit amet libero. Sed luctus euismod aliquet. Pellentesque commodo tempor quam, id dictum sem facilisis sit amet. Vestibulum non vestibulum ex. Mauris sit amet tristique ipsum, non faucibus lorem. Pellentesque at mi pharetra, eleifend libero eu, laoreet ex. Vivamus vulputate porttitor ultrices. Proin auctor aliquam tortor nec dignissim. Nam aliquam facilisis tellus ac sodales. Phasellus at molestie enim. Suspendisse a nisi commodo, posuere nisi eu, ornare neque. Integer nibh felis, fermentum a ultrices sit amet, viverra ac lorem. Quisque eget ultrices arcu. Quisque in scelerisque ipsum. Pellentesque malesuada aliquam erat vel ultricies. Vestibulum euismod, velit eu viverra sodales, dolor leo tempus felis, eu molestie libero arcu quis justo. In nec varius ligula. Sed nec dui vel est interdum lobortis vitae quis dui. Nunc ut scelerisque orci. Etiam accumsan condimentum nunc, sit amet iaculis quam interdum malesuada. Nullam blandit dolor in diam consectetur suscipit. Proin lacus urna, condimentum vitae odio nec, gravida tempor mi. In augue diam, aliquam et ligula a, interdum finibus urna. Proin nec ipsum mollis, lacinia lacus nec, vulputate velit. Sed euismod eleifend rutrum. Sed quis velit finibus, posuere neque blandit, rhoncus tortor. Donec pulvinar pharetra odio, imperdiet tincidunt sapien dapibus vitae. Aliquam vitae metus dignissim, imperdiet elit ut, dictum libero. Mauris sit amet tellus vitae est porta lacinia eget ut mauris. Proin tristique dictum luctus. Nulla dui metus, fermentum sit amet feugiat sit amet, bibendum facilisis urna. Fusce vel magna et nulla sodales faucibus. Praesent fringilla scelerisque lobortis.";
 
 let countWord = longText.trim().split(/\s+/).length;
@@ -50,6 +54,7 @@ let countNb = (longText.match(/( et )/g) || []).length
 console.log(countNb);
 
 
+// Bonus 2:
 let phraseToCheck = "taco cat";
 let phraseWithOutSpace = phraseToCheck.replace(/\s/g, '');
 
